9c1e140c864f812296f37e1d8398d106091d059e
BlakeAllred <blake.allred95@icloud.com>
2020-10-02 16:57:01 -0600

    added functions for drawing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const startDrawing = () => {
+  const canvasRef =  useRef(null)
+  const contextRef = useRef(null)
+  const [isDrawing, setDrawing] = useState(false)
+
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2
+    canvas.height = window.innerHeight * 2
+    canvas.style.width = `${window.innerWidth}px`
+    canvas.style.height = `${window.innerHeight}px`
+
+    const context = canvas.getContext('2d')
+    context.scale(2,2)
+    context.lineCap= 'round'
+    context.strokeStyle = 'black'
+    context.lineWidth= 5
+  }, [])
+
+  const startDrawing = ({nativeEvent}) => {
+    const {offsetX, offsetY} = nativeEvent
+    contextRef.current.beingPath()
+    contextRef.current.moveTo(offsetX, offsetY)
+    isDrawing(true)
+  }
+
+  const finishDrawing = () => {
+    isDrawing(false)
+  }
+
+  const draw = () => {
 
   }
 
@@ -12,6 +42,9 @@ function App() {
     <div className="App">
       <canvas
       onMouseDown={startDrawing}
+      onMouseUp = {finishDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}
       />
     </div>
   );
