d0b65e4b50473d859710ace1aa1bb018839c6f9a
BlakeAllred <blake.allred95@icloud.com>
2020-10-02 17:04:19 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,52 +1,54 @@
-import React, { useEffect, useRef, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
 
 function App() {
 
-  const canvasRef =  useRef(null)
+  const canvasRef = useRef(null)
   const contextRef = useRef(null)
-  const [isDrawing, setDrawing] = useState(false)
-
+  const [isDrawing, setIsDrawing] = useState(false)
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    canvas.width = window.innerWidth * 2
-    canvas.height = window.innerHeight * 2
-    canvas.style.width = `${window.innerWidth}px`
-    canvas.style.height = `${window.innerHeight}px`
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
 
-    const context = canvas.getContext('2d')
+    const context = canvas.getContext("2d")
     context.scale(2,2)
-    context.lineCap= 'round'
-    context.strokeStyle = 'black'
-    context.lineWidth= 5
+    context.lineCap = "round"
+    context.strokeStyle = "black"
+    context.lineWidth = 5
+    contextRef.current = context;
   }, [])
 
   const startDrawing = ({nativeEvent}) => {
-    const {offsetX, offsetY} = nativeEvent
-    contextRef.current.beingPath()
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.beginPath()
     contextRef.current.moveTo(offsetX, offsetY)
-    isDrawing(true)
+    setIsDrawing(true)
   }
 
   const finishDrawing = () => {
-    isDrawing(false)
+   contextRef.current.closePath()
+   setIsDrawing(false)
   }
 
-  const draw = () => {
-
+  const draw = ({nativeEvent}) => {
+    if(!isDrawing){
+      return
+    }
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY)
+    contextRef.current.stroke()
   }
 
   return (
-    <div className="App">
-      <canvas
+    <canvas
       onMouseDown={startDrawing}
-      onMouseUp = {finishDrawing}
+      onMouseUp={finishDrawing}
       onMouseMove={draw}
       ref={canvasRef}
-      />
-    </div>
+    />
   );
 }
 
